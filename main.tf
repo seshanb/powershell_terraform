ab90a9e8a3735da54f4222defc61f44194b9966d
seshanb <125367985+seshanb@users.noreply.github.com>
2023-08-09 17:36:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,16 @@
-variable "resource_group_name" {
-  description = "Name of the Azure Resource Group"
-  type        = string
+resource "azurerm_resource_group" "example" {
+  name     = var.resource_group_name
+  location = "East US"
+}
+
+data "local_file" "tfstate" {
+  filename = "${path.module}/terraform.tfstate"
+}
+
+data "terraform_remote_state" "stack" {
+  backend = "local"
+
+  config = {
+    path = data.local_file.tfstate.content != "" ? "${path.module}/terraform.tfstate" : "${path.module}/.terraform/terraform.tfstate"
+  }
 }
