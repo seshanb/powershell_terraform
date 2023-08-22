c4edbd5cf06c60c5d05e34895f870b48a84f74e7
seshanb <125367985+seshanb@users.noreply.github.com>
2023-08-22 15:50:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,25 @@ resource "azurerm_resource_group" "example" {
   location = "East US"
 }
 
+resource "null_resource" "execute_powershell" {
+  provisioner "local-exec" {
+    command = <<-EOT
+      $randomValue = Get-Random -Minimum 100 -Maximum 1000
+      $randomValue | Out-File random_output.txt
+    EOT
+    interpreter = ["pwsh", "-Command"]
+    working_dir = path.module
+  }
+
+  depends_on = [
+    azurerm_resource_group.example,
+  ]
+}
+
+output "random_value_output" {
+  value = file("random_output.txt")
+}
+
 data "local_file" "tfstate" {
   filename = "${path.module}/terraform.tfstate"
 }
